93fc4376068665dad81f4544318e730c385fde0b
Aart <aart_odding@hotmail.com>
2020-11-08 12:24:11 +0100

    Added TextData OutputNode

---
+++ ZigZagStandardLibrary/Text/TextData/TextData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ZigZag
+{
+    namespace Text
+    {
+        public class TextData : OutputNode
+        {
+
+            public List<string> Lines = new List<string>();
+
+        }
+    }
+}
