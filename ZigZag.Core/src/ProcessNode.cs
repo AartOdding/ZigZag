3dfebb8c9f0e3f1ad37fa7a39a6a41ed473664d6
Aart <aart_odding@hotmail.com>
2020-12-06 16:38:51 +0100

    Create AbstractExecutor class

--- ZigZag.Core/src/ProcessNode.cs
+++ ZigZag.Core/src/ProcessNode.cs
@@ -1,13 +1,36 @@
-
+﻿
 namespace ZigZag.Core
 {
     public class ProcessNode : AbstractNode
     {
+        public AbstractExecutor Executor
+        {
+            get
+            {
+                return m_executor;
+            }
+
+            set
+            {
+                m_executor = value;
+
+                if (!(m_executor is null))
+                {
+                    m_executor.Node = this;
+                }
+            }
+        }
+
+        public void Execute()
+        {
+
+        }
 
         public virtual void Process()
         {
 
         }
 
+        private AbstractExecutor m_executor;
     }
 }
---
+++ ZigZagCore/src/AbstractExecutor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZigZag.Core
+{
+    public class AbstractExecutor
+    {
+        public ProcessNode Node
+        {
+            get;
+            internal set;
+        }
+
+    }
+}
