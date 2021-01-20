6c47ec6fbcda47862efa56fe20e81d7d0c09a05c
Aart <aart_odding@hotmail.com>
2021-01-20 01:42:16 +0100

    vbo and vao abstraction

--- ZigZag.OpenGL/Source/Buffer.cs
+++ ZigZag.OpenGL/Source/Buffer.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OpenTK.Graphics.OpenGL;
 using System.Runtime.InteropServices;
 
@@ -31,11 +27,33 @@ namespace ZigZag.OpenGL
 
         public void Bind()
         {
+            if (m_released)
+            {
+                throw new Exception("Cannot bind released object.");
+            }
             if (m_buffer == 0)
             {
                 m_buffer = GL.GenBuffer();
             }
+            if (m_buffer == 0)
+            {
+                throw new Exception("Failed to create Buffer Object");
+            }
             GL.BindBuffer(Target, m_buffer);
+        }
+
+        public void Release()
+        {
+            if (m_released)
+            {
+                throw new Exception("Cannot release object twice");
+            }
+            if (m_buffer != 0)
+            {
+                GL.DeleteBuffer(m_buffer);
+                m_buffer = 0;
+                m_released = true;
+            }
         }
 
         public void Allocate(int sizeInBytes)
@@ -60,12 +78,8 @@ namespace ZigZag.OpenGL
             }
         }
 
-        public void CleanUp()
-        {
-
-        }
-
         private int m_buffer = 0;
         private int m_bufferSizeBytes = 0;
+        private bool m_released = false;
     }
 }
---
+++ ZigZag.OpenGL/Source/VertexDataBinding.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using OpenTK.Graphics.OpenGL;
+
+
+namespace ZigZag.OpenGL
+{
+    public class VertexDataBinding
+    {
+        public void Bind()
+        {
+            if (m_released)
+            {
+                throw new Exception("Cannot bind released object.");
+            }
+            if (m_vertexArray == 0)
+            {
+                m_vertexArray = GL.GenVertexArray();
+            }
+            if (m_vertexArray == 0)
+            {
+                throw new Exception("Failed to create Vertex Array Object");
+            }
+            GL.BindVertexArray(m_vertexArray);
+        }
+
+        public void Release()
+        {
+            if (m_released)
+            {
+                throw new Exception("Cannot release object twice");
+            }
+            if (m_vertexArray != 0)
+            {
+                GL.DeleteVertexArray(m_vertexArray);
+                m_vertexArray = 0;
+                m_released = true;
+            }
+        }
+
+        public void AttributeFloat(
+            int index,
+            int componentCount,
+            Buffer buffer,
+            VertexAttribPointerType bufferDataType,
+            int strideInBytes = 0,
+            int offsetInBufferInBytes = 0)
+        {
+            AttributeFloat(index, componentCount, buffer, bufferDataType, strideInBytes, offsetInBufferInBytes);
+        }
+
+        public void AttributeFloat(
+            int index,
+            int componentCount,
+            Buffer buffer,
+            VertexAttribPointerType bufferDataType,
+            bool normalize = false,
+            int strideInBytes = 0,
+            int offsetInBufferInBytes = 0)
+        {
+            if (buffer is null || buffer.Target != BufferTarget.ArrayBuffer)
+            {
+                throw new ArgumentException("Invalid buffer.");
+            }
+            Bind();
+            buffer.Bind();
+            GL.VertexAttribPointer(index, componentCount, bufferDataType, normalize, strideInBytes, offsetInBufferInBytes);
+            GL.EnableVertexAttribArray(index);
+        }
+
+        public void AttributeInt(
+            int index,
+            int componentCount,
+            Buffer buffer,
+            VertexAttribIntegerType bufferDataType,
+            int strideInBytes = 0,
+            int offsetInBufferInBytes = 0)
+        {
+            if (buffer is null || buffer.Target != BufferTarget.ArrayBuffer)
+            {
+                throw new ArgumentException("Invalid buffer.");
+            }
+            Bind();
+            buffer.Bind();
+            GL.VertexAttribIPointer(index, componentCount, bufferDataType, strideInBytes, (IntPtr)offsetInBufferInBytes);
+            GL.EnableVertexAttribArray(index);
+        }
+
+        private int m_vertexArray;
+        private bool m_released = false;
+    }
+}
