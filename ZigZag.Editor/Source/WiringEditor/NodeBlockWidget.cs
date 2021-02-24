944174f6fa45809aa1376a0a8ddd172e00b96483
Aart <aart_odding@hotmail.com>
2021-02-24 23:29:14 +0100

    Added NodeConnectorWidget

--- ZigZag.Editor/Source/WiringEditor/NodeBlockWidget.cs
+++ ZigZag.Editor/Source/WiringEditor/NodeBlockWidget.cs
@@ -12,16 +12,35 @@ namespace ZigZag.Editor.WiringEditor
 
             GeometryBuilder builder = new GeometryBuilder();
 
-            builder.Color = new Color(20, 20, 60, 255);
+            builder.Color = new Color(35, 35, 35, 255);
             builder.FillMode = FillMode.Filled;
             builder.AddRectangle(new Rectangle(-100, -70, 200, 140));
 
             builder.LinePlacement = LinePlacement.Centered;
-            builder.Color = new Color(160, 160, 160, 50);
+            builder.Color = new Color(60, 60, 60, 255);
             builder.FillMode = FillMode.Outline;
             builder.AddRectangle(new Rectangle(-100, -70, 200, 140));
 
             Geometry = builder.Build();
+
+            var i1 = new NodeConnectorWidget(true, 1);
+            var i2 = new NodeConnectorWidget(true, 1);
+            i1.Position = new Vector2(-105, -20);
+            i2.Position = new Vector2(-105, 20);
+            AddChild(i1);
+            AddChild(i2);
+
+            var o1 = new NodeConnectorWidget(false, 1);
+            var o2 = new NodeConnectorWidget(false, 1);
+            o1.Position = new Vector2(105, -20);
+            o2.Position = new Vector2(105, 20);
+            AddChild(o1);
+            AddChild(o2);
+        }
+
+        public override bool Contains(Vector2 point)
+        {
+            return base.Contains(point) || GetChildrenAt(point).Count > 0;
         }
 
         private Dragger dragger;
---
+++ ZigZag.Editor/Source/WiringEditor/NodeConnectorWidget.cs
@@ -0,0 +1,40 @@
+﻿using ZigZag.Mathematics;
+using ZigZag.SceneGraph;
+
+
+namespace ZigZag.Editor.WiringEditor
+{
+    class NodeConnectorWidget : GeometryNode
+    {
+        public NodeConnectorWidget(bool isLeftSide, int type)
+        {
+            m_leftSide = isLeftSide;
+            m_type = type;
+
+            GeometryBuilder builder = new GeometryBuilder();
+
+            builder.LinePlacement = LinePlacement.Centered;
+            builder.Color = new Color(60, 60, 60, 255);
+            builder.FillMode = FillMode.Outline;
+            builder.AddRectangle(new Rectangle(-5, -8, 10, 16));
+
+            Geometry = builder.Build();
+
+            MouseButtonPressEvent += OnMousePressEvent;
+            MouseButtonDragEvent += OnMouseDragEvent;
+        }
+
+        private void OnMousePressEvent(MouseButtonPressEvent e)
+        {
+            e.SetAccepted(e.Button == MouseButton.Left);
+        }
+
+        private void OnMouseDragEvent(MouseButtonDragEvent e)
+        {
+            System.Console.WriteLine("drag");
+        }
+
+        private bool m_leftSide;
+        private int m_type;
+    }
+}
