ac6ae92572a3e553a5ac2c73fa4812f422d77942
Aart <aart_odding@hotmail.com>
2021-02-24 22:44:28 +0100

    Removed some unused using statements

--- ZigZag.SceneGraph/Source/GeometryNode.cs
+++ ZigZag.SceneGraph/Source/GeometryNode.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ZigZag.Mathematics;
+﻿using ZigZag.Mathematics;
+
 
 namespace ZigZag.SceneGraph
 {
